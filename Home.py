237a3bd42b72a7c9d5d98ba36f5beabf47f3bde0
Wiem Ben Rim <wiem.rim.23@ucl.ac.uk>
2025-04-12 22:24:12 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,13 +5,8 @@ st.set_page_config(
     page_title="Welcome",
     page_icon="✨",
     layout="wide",
-    initial_sidebar_state="collapsed",
-   
+    # initial_sidebar_state="collapsed",
 )
-
-
-# pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
-# pg.run()
 
 # Load custom CSS
 with open('style.css') as f:
@@ -30,19 +25,23 @@ def main():
     # Create three columns for a modern layout
     col1, col2 = st.columns([1,1])
 
+
     with col1:
         st.markdown("""
             <div class="card">
-                <h3>Learn the basiscs</h3>
-                <p>Discover how AI facial recognition works and why bias can occur</p>
+                <h2 style="color:black">Facial Recognition</h2>
+                <p> Discover how AI facial recognition works and why bias happens with our interactive AI predictor! </p>
+                <img src="https://media.canva.com/v2/files/uri:ifs%3A%2F%2FM%2Ff7a76fe6-3dd5-45a0-8858-964b5d1f1519?csig=AAAAAAAAAAAAAAAAAAAAAMmFB-ZPhcGW0LoMAk97dW95BqsrQ4AItNI5Ip4pTVBw&exp=1744507053&signer=media-rpc&token=AAIAAU0AJGY3YTc2ZmU2LTNkZDUtNDVhMC04ODU4LTk2NGI1ZDFmMTUxOQAAAAABliy5I8hCHhSgRryTOrU7beSAXYeMZkxkDrtb6_gEJk91M_PTtg" alt="Avatar" style="width:100%;border-radius: 20px; margin-top: 10px;">
+            
             </div>
         """, unsafe_allow_html=True)
 
     with col2:
         st.markdown("""
             <div class="card">
-                <h3>Fact-checking</h3>
-                <p>Check other pages for more examples</p>
+                <h2 style="color:black">Fact-checking</h2>
+                <p> Interract with a chatbot and ask it some of our suggested questions, see if these tools can make mistakes!</p>
+                <img src="https://media.canva.com/v2/files/uri:ifs%3A%2F%2FM%2Fc1d3b292-ffd2-4c7d-8a73-a199bd9607de?csig=AAAAAAAAAAAAAAAAAAAAALjo0qJT2Oxw7L9MzrxIEOXTNESeowF1YVAXh976IbYI&exp=1744506393&signer=media-rpc&token=AAIAAU0AJGMxZDNiMjkyLWZmZDItNGM3ZC04YTczLWExOTliZDk2MDdkZQAAAAABliyvEajIqZ1TJSLqjIxd9u5WUNv2b57csGHWpUd6fF_v3o4T3Q" alt="Avatar" style="width:100%;border-radius: 20px; margin-top: 10px;">
             </div>
         """, unsafe_allow_html=True)
 
